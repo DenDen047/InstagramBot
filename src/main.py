403bb0e82ebe73857a08150966f4a22e1fe17f44
DenDen047 <sh.mn.nat@gmail.com>
2019-04-30 20:39:47 +0800

    instagram apiが使えることが確認できた

--- src/main.py
+++ src/main.py
@@ -2,12 +2,27 @@ import os
 import sys
 import re
 import glob
+import json
 
-import cv2
+from InstagramAPI import InstagramAPI
 
 
 def main():
-    pass
+    # アカウント情報をロード
+    with open('account_info.json', 'r') as f:
+        data = json.load(f)
+        login = data['login']
+        password = data['password']
+
+    # ログインテスト
+    api = InstagramAPI(login, password)
+    if (api.login()):
+        api.getSelfUserFeed()  # get self user feed
+        # print(api.LastJson)  # print last response JSON
+        print("Login succes!")
+    else:
+        print("Can't login!")
+
 
 
 if __name__ == '__main__':
